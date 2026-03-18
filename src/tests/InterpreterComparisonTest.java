97a401992a855e26f7f51ea8dbe22a6383fdc5a3
Estiby <estiwnl@gmail.com>
2026-03-17 18:57:59 -0600

    Move the files to Test folder

--- src/tests/InterpreterComparisonTest.java
+++ src/tests/InterpreterComparisonTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collection;
 
 public class InterpreterComparisonTest extends InterpreterTestBase {
 
